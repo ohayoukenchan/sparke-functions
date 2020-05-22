7e2e05d6e4452e73b12b14e1df7553552891c8fe
ohayoukenchan <san.kun.sakana@gmail.com>
2020-05-22 15:45:38 +0900

    ✨ bookの追加API作成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,68 @@ const corsHandler = cors({ origin: true });
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+
+// データベースの参照を作成
+var fireStore = admin.firestore()
+
+exports.addBooks = functions.https.onRequest((request, response) => {
+  // 動作確認のため適当なデータをデータベースに保存
+  const booksRef = fireStore.collection('books');
+  booksRef.doc('9784334751081').set({
+    isbn: '9784334751081', 
+    price: '770', 
+    publisher: '光文社',
+    title: '永遠平和のために/啓蒙とは何か 他3編 (光文社古典新訳文庫) (日本語) 文庫'
+  })
+
+  corsHandler(request, response, () => {
+    const bookRef = fireStore.collection('books').doc('9784334751081')
+    bookRef.get()
+      .then(doc => {
+        if (!doc.exists) {
+          response.send('No such document!')
+        } else {
+          response.send(doc.data())
+        }
+      })
+      .catch(err => {
+        response.send('not found')
+      })
+    })
+});
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  // 動作確認のため適当なデータをデータベースに保存
+  var citiesRef = fireStore.collection('cities');
+  citiesRef.doc('SF').set({
+    name: 'San Francisco', state: 'CA', country: 'USA',
+    capital: false, population: 860000
+  })
+
+  var cityRef = fireStore.collection('cities').doc('SF')
+  cityRef.get()
+    .then(doc => {
+      if (!doc.exists) {
+        response.send('No such document!')
+      } else {
+        response.send(doc.data())
+      }
+    })
+    .catch(err => {
+      response.send('not found')
+    })
+})
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  const snapshot = await admin.database().ref('/messages').push({ original: original });
+  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  res.redirect(303, snapshot.ref.toString());
+});
+
+
 exports.addMessage = functions.https.onRequest(async (req, res) => {
     // Grab the text parameter.
     const original = req.query.text;
